ab2487a9f4d3fd66ed1e944be555735f0844d1b4
Hugo Abreu <hugo.abreu@tomtom.com>
2017-11-10 16:28:50 +0000

    Replaced index.ios.js and index.android.js with a joint index.js.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-/**
- * @flow
- */
+/* @flow */
 'use strict'
 
 import { NativeModules, NativeEventEmitter } from 'react-native'
 import type EmitterSubscription from 'EmitterSubscription'
+import { Platform } from 'react-native';
 
 export type SafeAreaInsets = { top: number, left: number, bottom: number, right: number };
 
@@ -13,16 +12,31 @@ const nativeEventEmitter = new NativeEventEmitter(nativeModule)
 
 class SafeArea {
   getSafeAreaInsetsForRootView(): Promise<{ safeAreaInsets: SafeAreaInsets }> {
-    return nativeModule.getSafeAreaInsetsForRootView()
+    if (Platform.OS === 'ios') {
+      // iOS
+      return nativeModule.getSafeAreaInsetsForRootView();
+    } else {
+      // Android
+      return Promise.resolve({ safeAreaInsets: { top: 0, left: 0, bottom: 0, right: 0 } });
+    }
   }
 
   addEventListener(eventType: string, listener: Function, context: ?Object): ?EmitterSubscription {
-    return nativeEventEmitter.addListener(eventType, listener, context)
+    if (Platform.OS === 'ios') {
+      // iOS
+      return nativeEventEmitter.addListener(eventType, listener, context);
+    } else {
+      // Android
+      return null;
+    }
   }
 
   removeEventListener(eventType: string, listener: Function): void {
-    nativeEventEmitter.removeListener(eventType, listener)
+    if (Platform.OS === 'ios') {
+      // iOS
+      nativeEventEmitter.removeListener(eventType, listener)
+    }
   }
 }
 
-export default new SafeArea()
+export default new SafeArea();
